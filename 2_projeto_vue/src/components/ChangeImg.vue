853c5d294f4b8d9bebfaa37d1370f8ab7979f217
Klysman08 <klysman.rk@gmail.com>
2022-07-25 20:09:01 -0300

    emit function

---
+++ 2_projeto_vue/src/components/ChangeImg.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <button @click="$emit('trocar')">Change the Image</button>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'ChangeImg',
+        emits: ['trocar'],
+    }
+</script>
--- 2_projeto_vue/src/components/Picture.vue
+++ 2_projeto_vue/src/components/Picture.vue
@@ -1,16 +1,43 @@
 <template>
     <img :src="avatar" :alt="description">
+    <ChangeImgVue @trocar="changeImg"></ChangeImgVue>
+
 </template>
 
 <script>
+    import ChangeImgVue from './ChangeImg.vue'
     export default {
         name: 'Picture',
+        components: {
+            ChangeImgVue
+        },
         data() {
             return {
-                avatar: "/img/preview.jpg",
-                description: "This is a description"
+                avatar: "/img/nayon.jpg",
+                description: "This is a description",
+                ativado: false
+            }
+        },
+        methods: {
+            changeImg() {
+                if (this.ativado) {
+                    this.avatar = "/img/nayon.jpg"
+                    this.description = "This is a description"
+                    this.ativado = false
+                } else {
+                    this.avatar = "/img/nayon2.jpg"
+                    this.description = "This is a description 2"
+                    this.ativado = true
+                }
             }
         }
     }
 </script>
 
+<style scoped> 
+    img {
+        width: 50%;
+        height: 50%;
+        margin: 20px;
+    }
+</style>
