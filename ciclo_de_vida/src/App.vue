af44f7968602328a6a648c5864597b17c5037e57
klysman08 <klysman.rk@gmail.com>
2022-09-03 19:52:41 -0300

    Before Update and Update

--- ciclo_de_vida/src/App.vue
+++ ciclo_de_vida/src/App.vue
@@ -3,6 +3,7 @@
         <h1>Hello word</h1>
         <BeforeCreate_and_Created></BeforeCreate_and_Created>
         <BeforeMount_Mounted></BeforeMount_Mounted>
+        <BeforeUpdate_Update></BeforeUpdate_Update>
     </div>
 </template>
 
@@ -10,12 +11,14 @@
 import { defineComponent } from 'vue';
 import BeforeCreate_and_Created from './components/BeforeCreate_and_Created.vue';
 import BeforeMount_Mounted from './components/BeforeMount_Mounted.vue';
+import BeforeUpdate_Update from './components/BeforeUpdate_Update.vue';
 
 export default defineComponent({
     name: 'App',
     components: {
         BeforeCreate_and_Created,
-        BeforeMount_Mounted
+        BeforeMount_Mounted,
+        BeforeUpdate_Update
     }
 });
 </script>
---
+++ ciclo_de_vida/src/components/BeforeUpdate_Update.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+        <h2>{{ count }}</h2>
+        <button @click="count++">Increment</button>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: 'BeforeUpdate_Update',
+    data() {
+        return {
+            message: 'Before Update and Update',
+            count: 0
+        };
+    },
+    // ambos os métodos possuem acesso aos dados e a DOM.
+    beforeUpdate() {
+        //console.log('beforeUpdate');
+    },
+    updated() {
+        console.log('update');
+        if (this.count > 5) {
+            this.count = 0;
+        }
+    }
+});
+</script>
