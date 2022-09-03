cb01377082efc07706f8d020f4498f2bf2bee71d
klysman08 <klysman.rk@gmail.com>
2022-09-03 19:07:39 -0300

    Methods Before Created and Created

---
+++ ciclo_de_vida/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Hello word</h1>
+        <BeforeCreate_and_Created></BeforeCreate_and_Created>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BeforeCreate_and_Created from './components/BeforeCreate_and_Created.vue';
+
+export default defineComponent({
+    name: 'App',
+    components: {
+        BeforeCreate_and_Created
+    }
+});
+</script>
+
+<style></style>
---
+++ ciclo_de_vida/src/components/BeforeCreate_and_Created.vue
@@ -0,0 +1,24 @@
+<template lang="">
+    <div>
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: 'BeforeCreate_and_Created',
+    data() {
+        return {
+            message: 'Hello Created'
+        };
+    },
+    beforeCreate() {
+        console.log(this.message); // Ainda não pudemos acessar o data
+    },
+    created() {
+        console.log(this.message); // Depois que o componente é criado, podemos acessar o data
+        // é o momento mais adequado para fazer requisições HTTP, mas esse método ainda não tem acesso ao DOM.
+    }
+});
+</script>
