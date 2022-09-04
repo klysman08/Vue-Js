b35a3f0157bb037d5d92666f2d3af61642e82b49
klysman08 <klysman.rk@gmail.com>
2022-09-03 22:32:12 -0300

    Diretiva v-if-else and v-show

--- diretivas/src/App.vue
+++ diretivas/src/App.vue
@@ -4,6 +4,7 @@
         <Diretiva_vhtml></Diretiva_vhtml>
         <Diretiva_von></Diretiva_von>
         <Diretiva_vonce></Diretiva_vonce>
+        <Diretiva_vif></Diretiva_vif>
     </div>
 </template>
 
@@ -13,6 +14,7 @@ import Diretiva_vbind from './components/Diretiva_vbind.vue';
 import Diretiva_vhtml from './components/Diretiva_vhtml.vue';
 import Diretiva_von from './components/Diretiva_von.vue';
 import Diretiva_vonce from './components/Diretiva_vonce.vue';
+import Diretiva_vif from './components/Diretiva_vif.vue';
 
 export default defineComponent({
     name: 'App',
@@ -20,7 +22,67 @@ export default defineComponent({
         Diretiva_vbind,
         Diretiva_vhtml,
         Diretiva_von,
-        Diretiva_vonce
+        Diretiva_vonce,
+        Diretiva_vif
     }
 });
 </script>
+
+<style>
+/* From uiverse.io by @kirzin */
+button {
+    font: bold;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif, uppercase;
+    text-decoration: none;
+    position: absolute;
+    border: none;
+    font-size: 14px;
+    font-family: inherit;
+    color: #fff;
+    width: 9em;
+    height: 3em;
+    line-height: 2em;
+    text-align: center;
+    background: linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4);
+    background-size: 300%;
+    border-radius: 30px;
+    z-index: 1;
+}
+
+button:hover {
+    animation: ani 8s linear infinite;
+    border: none;
+}
+
+@keyframes ani {
+    0% {
+        background-position: 0%;
+    }
+
+    100% {
+        background-position: 400%;
+    }
+}
+
+button:before {
+    content: '';
+    position: absolute;
+    top: -5px;
+    left: -5px;
+    right: -5px;
+    bottom: -5px;
+    z-index: -1;
+    background: linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4);
+    background-size: 400%;
+    border-radius: 35px;
+    transition: 1s;
+}
+
+button:hover::before {
+    filter: blur(20px);
+}
+
+button:active {
+    background: linear-gradient(32deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4);
+}
+</style>
---
+++ diretivas/src/components/Diretiva_vif.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div>..</div>
+        <h1>Diretiva v-if</h1>
+        <h2 v-if="sex === 'M'">Masculino</h2>
+        <h2 v-else-if="sex === 'F'">Feminino</h2>
+        <h3 v-else>Outro</h3>
+        <h1>Diretiva v-show</h1>
+        <h2 v-show="sex === 'F'">Feminino</h2>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Diretiva_vif',
+    data() {
+        return {
+            sex: 'F'
+        };
+    }
+});
+</script>
