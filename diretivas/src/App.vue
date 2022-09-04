3dad36a6fb791f99347a9f7b80b9bcd61dc71f14
klysman08 <klysman.rk@gmail.com>
2022-09-03 21:37:47 -0300

    Diretivas and v-bind

---
+++ diretivas/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <Diretiva_vbind></Diretiva_vbind>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Diretiva_vbind from './components/Diretiva_vbind.vue';
+
+export default defineComponent({
+    name: 'App',
+    components: {
+        Diretiva_vbind
+    }
+});
+</script>
---
+++ diretivas/src/components/Diretiva_vbind.vue
@@ -0,0 +1,45 @@
+<template lang="">
+    <div>
+        <h1>Directiva v-bind</h1>
+        <p v-bind:title="titulo">{{ titulo }}</p>
+        <!-- v-for para minha lista de produtos -->
+        <div v-for="produto in produtos" :key="produto.id">
+            <!-- <img :src="produto.url" :alt="produto.nome" :title="produto.nome" /> -->
+            <h2>{{ produto.name }}</h2>
+            <p>{{ produto.price }}</p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: 'Diretiva_vbind',
+    data() {
+        return {
+            titulo: 'Título do elemento',
+            // um objeto de produtos com nome url e preço
+            produtos: [
+                {
+                    id: 1,
+                    name: 'Produto 1',
+                    url: 'https://picsum.photos/200/300',
+                    price: 10
+                },
+                {
+                    id: 2,
+                    name: 'Produto 2',
+                    url: 'https://picsum.photos/200/300',
+                    price: 20
+                },
+                {
+                    id: 3,
+                    name: 'Produto 3',
+                    url: 'https://picsum.photos/200/300',
+                    price: 30
+                }
+            ]
+        };
+    }
+});
+</script>
